ff0eaebd6d715ab147e59a502969c227ff3deb28
Dominique Vernier <dvernier@redhat.com>
2020-07-01 11:15:00 -0400

    Clean code smell

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -127,7 +127,8 @@ func HaveServerResources(client clientset.Interface, expectedAPIGroups []string)
 	return nil
 }
 
-//Deprecated: Use https://github.com/open-cluster-management/library-go/pkg/apis/meta/v1/crd#HaveCRDs
+//Deprecated:
+// Use https://github.com/open-cluster-management/library-go/pkg/apis/meta/v1/crd#HaveCRDs
 //HaveCRDs returns an error if all provided CRDs are not installed
 //client: the client to use
 //expectedCRDs: The list of expected CRDS to find
@@ -135,7 +136,8 @@ func HaveCRDs(client clientset.Interface, expectedCRDs []string) error {
 	return libgocrdv1.HaveCRDs(client, expectedCRDs)
 }
 
-//Deprecated: Use https://github.com/open-cluster-management/library-go/pkg/apis/meta/v1/deployment#HaveDeploymentsInNamespace
+//Deprecated:
+// Use https://github.com/open-cluster-management/library-go/pkg/apis/meta/v1/deployment#HaveDeploymentsInNamespace
 //HaveDeploymentsInNamespace returns an error if all provided deployment are not installed in the given namespace
 //client: the client to use
 //namespace: The namespace to search in
