122c44798bd47e568689282d1b80fc12311bf8a3
Ian Zhang <izhang@redhat.com>
2020-07-29 10:22:30 -0400

    delete the copy right lines
    Signed-off-by: Ian Zhang <izhang@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -3,6 +3,9 @@
 export GO111MODULE=off
 
 # Go tools
+_OS=$(go env GOOS)
+_ARCH=$(go env GOARCH)
+KubeBuilderVersion="2.3.1"
 
 if ! which patter > /dev/null; then      echo "Installing patter ..."; go get -u github.com/apg/patter; fi
 if ! which gocovmerge > /dev/null; then  echo "Installing gocovmerge..."; go get -u github.com/wadey/gocovmerge; fi
@@ -10,19 +13,18 @@ if ! which golangci-lint > /dev/null; then
    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.23.6
 fi
 
-# Install kubebuilder for unit test
-echo "Install Kubebuilder components for test framework usage!"
+if ! which kubebuilder > /dev/null; then
+   # Install kubebuilder for unit test
+   echo "Install Kubebuilder components for test framework usage!"
 
-_OS=$(go env GOOS)
-_ARCH=$(go env GOARCH)
-KubeBuilderVersion="2.3.1"
-# download kubebuilder and extract it to tmp
-curl -L https://go.kubebuilder.io/dl/"$KubeBuilderVersion"/"${_OS}"/"${_ARCH}" | tar -xz -C /tmp/
+   # download kubebuilder and extract it to tmp
+   curl -L https://go.kubebuilder.io/dl/"$KubeBuilderVersion"/"${_OS}"/"${_ARCH}" | tar -xz -C /tmp/
 
-# move to a long-term location and put it on your path
-# (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-sudo mv /tmp/kubebuilder_"$KubeBuilderVersion"_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
-export PATH=$PATH:/usr/local/kubebuilder/bin
+   # move to a long-term location and put it on your path
+   # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
+   sudo mv /tmp/kubebuilder_"$KubeBuilderVersion"_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
+   export PATH=$PATH:/usr/local/kubebuilder/bin
+fi
 
 # Build tools
 
