e6c661d214ee23faef918dd375a702249eaa4f0c
Dominique Vernier <dvernier@redhat.com>
2021-03-04 10:59:04 -0500

    Use OPENSHIFT_CI env var
    Signed-off-by: Dominique Vernier <dvernier@redhat.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -23,11 +23,11 @@ if ! which kubebuilder > /dev/null; then
 
    # move to a long-term location and put it on your path
    # (you'll need to set the KUBEBUILDER_ASSETS env var if you put it somewhere else)
-   echo "PROW_JOB_ID: "$PROW_JOB_ID
-   if [ -z $PROW_JOB_ID ]; then
-     sudo mv /tmp/kubebuilder_"$KubeBuilderVersion"_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
-   else 
+   echo "OPENSHIFT_CI: "$OPENSHIFT_CI
+   if [ $OPENSHIFT_CI == "true" ]; then
      mv /tmp/kubebuilder_"$KubeBuilderVersion"_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
+   else 
+     sudo mv /tmp/kubebuilder_"$KubeBuilderVersion"_"${_OS}"_"${_ARCH}" /usr/local/kubebuilder
    fi
 fi
 
